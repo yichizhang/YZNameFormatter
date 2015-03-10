a21f77f89064b016a3b30e805ac1717ed078ae1e
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-03-11 02:18:00 +1100

    Added code to extract title/prefix

--- Source/YZNameFormatter.swift
+++ Source/YZNameFormatter.swift
@@ -5,8 +5,11 @@
 //  Created by Yichi on 10/03/2015.
 //  Copyright (c) 2015 Yichi. All rights reserved.
 //
+
 /*
+
 (.*)Prof(\S)*\s
+
 ((.*)Prof(\S)*\s|Miss|Mrs|Mr|Dr)
 
 Assoc Professsor Eve Fallshaw
@@ -23,45 +26,32 @@ Dr Aaron Johnston
 
 import Foundation
 import UIKit
+import AddressBook
 
 class YZNameFormatter {
 	
-	let prefixDict = [
-		"prefix" : [
-			"Mr." : ["mr", "mister", "master"],
-			"Mrs." : ["mrs", "missus", "missis"],
-			"Ms." : ["ms", "miss"],
-			"Dr." : ["dr"],
-			"Rev." : ["rev", "rev'd", "reverend"],
-			"Fr." : ["fr", "father"],
-			"Sr." : ["sr", "sister"],
-			"Prof." : ["prof", "professor"],
-			"Sir" : ["sir"],
-			"" : "the"
-		]
-	]
-	
-	let compoundDict = [
-		"compound" : [
-			"vere","von","van","de","del","della","di","da","pietro","vanden","du","st.","st","la","ter"
-		]
-	]
-	
-	let suffixDict = [
-		"suffixes" : [
-			"line" : [ "I","II","III","IV","V","Senior","Junior","Jr","Sr" ],
-			"prof" : [ "PhD","APR","RPh","PE","MD","MA","DMD","CME" ]
-		]
-	]
-	
-	func nameFrom(#string:String) -> YZName {
+	func nameFrom(#string:String, error:AutoreleasingUnsafeMutablePointer<NSError?> = nil) -> YZName {
 		let name = YZName()
 		
-		var fullName = string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+		let fullName = string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as NSString
 		
-		var prefixArray = [
-			"mr", "mrs", "miss", "prof", "dr", "assoc professor"
-		]
+		var additionalPrefixArray = [ "mr", "mrs", "ms", "miss", "dr" ]
+		let additionalPrefixRegexString = "|".join(additionalPrefixArray)
+		let prefixRegexString = "((.*)Prof(\\S)*\\s|\(additionalPrefixRegexString))"
+		
+		if let prefixRegex = NSRegularExpression(pattern: prefixRegexString, options: NSRegularExpressionOptions.CaseInsensitive, error: error) {
+			prefixRegex.enumerateMatchesInString(
+				fullName,
+				options: NSMatchingOptions.allZeros,
+				range: fullName.rangeOfString(fullName),
+				usingBlock: { (result:NSTextCheckingResult!, flags:NSMatchingFlags, stop:UnsafeMutablePointer<ObjCBool>) -> Void in
+					name.prefix = fullName.substringWithRange(result.range).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+					stop.memory = true
+					return
+			})
+		}
+		
+		let compoundArray = ["vere","von","van","de","del","della","di","da","pietro","vanden","du","st.","st","la","ter"]
 		
 		return name
 	}
