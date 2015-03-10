705c9ac5a0b0e0fead1abe2d3e2151da3bb7d3e8
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-03-11 02:17:32 +1100

    Organised test cases; added more test cases

--- NameFormatterDemo/NameFormatterDemoTests/NameFormatterDemoTests.swift
+++ NameFormatterDemo/NameFormatterDemoTests/NameFormatterDemoTests.swift
@@ -21,7 +21,7 @@ class NameFormatterDemoTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testProfessorNames() {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
 		var nameString:String!
@@ -58,6 +58,74 @@ class NameFormatterDemoTests: XCTestCase {
 		XCTAssertEqual(name.firstName, "Harry", "1")
 		XCTAssertEqual(name.lastName, "McLean", "1")
 		
+		nameString = "Dr Harry Hamilton"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Dr", "1")
+		XCTAssertEqual(name.firstName, "Harry", "1")
+		XCTAssertEqual(name.lastName, "Hamilton", "1")
+		
+		nameString = "Dr Aaron Johnston"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Dr", "1")
+		XCTAssertEqual(name.firstName, "Aaron", "1")
+		XCTAssertEqual(name.lastName, "Johnston", "1")
+		
+		nameString = "Dr Vanessa Panettieri"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Dr", "1")
+		XCTAssertEqual(name.firstName, "Vanessa", "1")
+		XCTAssertEqual(name.lastName, "Panettieri", "1")
+		
+    }
+	
+	func testVietnameseVanNames() {
+		// This is an example of a functional test case.
+		XCTAssert(true, "Pass")
+		var nameString:String!
+		var name:YZName!
+		let formatter = YZNameFormatter()
+		
+		nameString = "Mr Brinh Tu Van"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Mr", "1")
+		XCTAssertEqual(name.firstName, "Brinh Tu", "1")
+		XCTAssertEqual(name.lastName, "Van", "1")
+		
+		nameString = "Ms Van Phung Thanh"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Ms", "1")
+		XCTAssertEqual(name.firstName, "Van Phung", "1")
+		XCTAssertEqual(name.lastName, "Thanh", "1")
+	}
+	
+	func testDutchVanNames() {
+		// This is an example of a functional test case.
+		XCTAssert(true, "Pass")
+		var nameString:String!
+		var name:YZName!
+		let formatter = YZNameFormatter()
+		
+		nameString = "Mr Mike Van Den Acker"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Mr", "1")
+		XCTAssertEqual(name.firstName, "Mike", "1")
+		XCTAssertEqual(name.lastName, "Van Den Acker", "1")
+		
+		nameString = "Ms Kane Van Oorschot"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Ms", "1")
+		XCTAssertEqual(name.firstName, "Kane", "1")
+		XCTAssertEqual(name.lastName, "Van Oorschot", "1")
+		
+	}
+
+	func testOtherNames() {
+		// This is an example of a functional test case.
+		XCTAssert(true, "Pass")
+		var nameString:String!
+		var name:YZName!
+		let formatter = YZNameFormatter()
+		
 		nameString = "Josh Hall"
 		name = formatter.nameFrom(string: nameString)
 		XCTAssertEqual(name.prefix, "", "1")
@@ -70,19 +138,46 @@ class NameFormatterDemoTests: XCTestCase {
 		XCTAssertEqual(name.firstName, "Harry", "1")
 		XCTAssertEqual(name.lastName, "Hall", "1")
 		
-		nameString = "Dr Harry Hamilton"
+		nameString = "Ms Courtney Sullivan"
 		name = formatter.nameFrom(string: nameString)
-		XCTAssertEqual(name.prefix, "Dr", "1")
-		XCTAssertEqual(name.firstName, "Harry", "1")
-		XCTAssertEqual(name.lastName, "Hamilton", "1")
+		XCTAssertEqual(name.prefix, "Ms", "1")
+		XCTAssertEqual(name.firstName, "Courtney", "1")
+		XCTAssertEqual(name.lastName, "Sullivan", "1")
 		
-		nameString = "Dr Aaron Johnston"
+		nameString = "Mr Jogvan Klein"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Mr", "1")
+		XCTAssertEqual(name.firstName, "Jogvan", "1")
+		XCTAssertEqual(name.lastName, "Klein", "1")
+		
+		nameString = "Mr Giovanni Colonna"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Mr", "1")
+		XCTAssertEqual(name.firstName, "Giovanni", "1")
+		XCTAssertEqual(name.lastName, "Colonna", "1")
+	}
+
+	func testComplexNames() {
+		// This is an example of a functional test case.
+		XCTAssert(true, "Pass")
+		var nameString:String!
+		var name:YZName!
+		let formatter = YZNameFormatter()
+		
+		nameString = "Dr Catherine Hall-Van Den Elsen"
 		name = formatter.nameFrom(string: nameString)
 		XCTAssertEqual(name.prefix, "Dr", "1")
-		XCTAssertEqual(name.firstName, "Aaron", "1")
-		XCTAssertEqual(name.lastName, "Johnston", "1")
-    }
-    
+		XCTAssertEqual(name.firstName, "Catherine", "1")
+		XCTAssertEqual(name.lastName, "Hall-Van Den Elsen", "1")
+		
+		nameString = "Mr James Van-Van Den Elsen"
+		name = formatter.nameFrom(string: nameString)
+		XCTAssertEqual(name.prefix, "Mr", "1")
+		XCTAssertEqual(name.firstName, "James", "1")
+		XCTAssertEqual(name.lastName, "Van-Van Den Elsen", "1")
+		
+	}
+	
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
