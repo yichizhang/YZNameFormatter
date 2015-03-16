08f996ae84e62328f8710a58391fbc9ca5b00cbc
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-03-16 12:39:59 +1100

    Changed a few variable names; added support for last names like "Hall-Van Den Acker"; added some comments.

--- Source/YZNameFormatter.swift
+++ Source/YZNameFormatter.swift
@@ -36,19 +36,23 @@ class YZNameFormatter {
 		let fullName = string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as NSString
 		var nameWithOutPrefix = fullName
 		
-		let additionalPrefixArray = [ "mr", "mrs", "ms", "miss", "dr" ]
-		let additionalPrefixRegexString = "|".join(additionalPrefixArray)
+		var regexPatternString:String
+		var a:[String]
+		var addtionalPatternString:String
+		
+		a = [ "mr", "mrs", "ms", "miss", "dr" ]
+		addtionalPatternString = "|".join(a)
 		
 		// ((.*)Prof(\S)*\s|(Miss|Mrs|Mr|Dr)\s)
-		let prefixRegexString = "((.*)Prof(\\S)*\\s|(\(additionalPrefixRegexString))\\s)"
-		
-		if let regex = NSRegularExpression(pattern: prefixRegexString, options: NSRegularExpressionOptions.CaseInsensitive, error: error) {
+		regexPatternString = "((.*)Prof(\\S)*\\s|(\(addtionalPatternString))\\s)"
+		if let regex = NSRegularExpression(pattern: regexPatternString, options: NSRegularExpressionOptions.CaseInsensitive, error: error) {
 			
 			regex.enumerateMatchesInString(
 				fullName,
 				options: NSMatchingOptions.allZeros,
 				range: fullName.rangeOfString(fullName),
 				usingBlock: { (result:NSTextCheckingResult!, flags:NSMatchingFlags, stop:UnsafeMutablePointer<ObjCBool>) -> Void in
+					
 					name.prefix = fullName.substringWithRange(result.range).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
 					nameWithOutPrefix = (fullName as String).stringByReplacingOccurrencesOfString(name.prefix, withString: "", options: NSStringCompareOptions.allZeros).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
 					stop.memory = true
@@ -56,10 +60,11 @@ class YZNameFormatter {
 			})
 		}
 		
-		let additionalCompoundArray = ["vere","von","van","de","del","della","di","da","pietro","vanden","du","st.","st","la","ter"]
-		let additionalCompoundRegexString = "|".join(additionalCompoundArray)
-		let compoundRegexString = "\\s(\(additionalCompoundRegexString))\\s"
-		if let regex = NSRegularExpression(pattern: compoundRegexString, options: NSRegularExpressionOptions.CaseInsensitive, error: error) {
+		a = ["vere","von","van","de","del","della","di","da","pietro","vanden","du","st.","st","la","ter"]
+		addtionalPatternString = "|".join(a)
+		
+		regexPatternString = "\\s(\(addtionalPatternString))\\s"
+		if let regex = NSRegularExpression(pattern: regexPatternString, options: NSRegularExpressionOptions.CaseInsensitive, error: error) {
 			
 			regex.enumerateMatchesInString(
 				nameWithOutPrefix,
@@ -75,15 +80,43 @@ class YZNameFormatter {
 		}
 		
 		if name.lastName.isEmpty == true {
+			// No "Van Den Acker" style last name is found
+			// Look for "Hall-Van Den Acker" style last name
+			
+			// \s\w*-\w*\s
+			regexPatternString = "\\s\\w*-\\w*\\s"
+			if let regex = NSRegularExpression(pattern: regexPatternString, options: NSRegularExpressionOptions.CaseInsensitive, error: error) {
+				
+				regex.enumerateMatchesInString(
+					nameWithOutPrefix,
+					options: NSMatchingOptions.allZeros,
+					range: nameWithOutPrefix.rangeOfString(nameWithOutPrefix),
+					usingBlock: { (result:NSTextCheckingResult!, flags:NSMatchingFlags, stop:UnsafeMutablePointer<ObjCBool>) -> Void in
+						
+						name.lastName = nameWithOutPrefix.substringFromIndex(result.range.location).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+						name.firstName = (nameWithOutPrefix as String).stringByReplacingOccurrencesOfString(name.lastName, withString: "", options: .allZeros).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+						stop.memory = true
+						return
+				})
+			}
+		}
+		
+		if name.lastName.isEmpty == true {
+			// No "Van Den Acker" or "Hall-Van Den Acker" style last name is found
+			
 			let lastWhiteSpaceRange = nameWithOutPrefix.rangeOfString(" ", options: NSStringCompareOptions.BackwardsSearch)
 			
 			if lastWhiteSpaceRange.location == NSNotFound {
 				if name.prefix.isEmpty == true {
+					// If no prefix is found, and no space is found, the string is probably first name only
 					name.firstName = nameWithOutPrefix
 				} else {
+					// If a prefix is found, and no space is found in the rest of the name, the string is probably
+					// a combination of prefix and last name, e.g. something like "Mr McLean"
 					name.lastName = nameWithOutPrefix
 				}
 			} else {
+				// A space is found. Take the portion behind the space as the last name and the rest as the first name.
 				name.firstName = nameWithOutPrefix.substringToIndex(lastWhiteSpaceRange.location)
 				name.lastName = nameWithOutPrefix.substringFromIndex(NSMaxRange(lastWhiteSpaceRange))
 			}
